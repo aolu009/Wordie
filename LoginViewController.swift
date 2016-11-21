dd284d2e824483d954796aecf62a168861fad65c
parry <parry@parrys-air.attlocal.net>
2016-11-21 14:58:06 -0800

    adding LoginVC

---
+++ LoginViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LoginViewController.swift
+//  Wordie
+//
+//  Created by parry on 11/21/16.
+//  Copyright © 2016 Lu Ao. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func onLoginButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func onSignUpButtonPressed(_ sender: Any) {
+    }
+    
+
+
+
+}
