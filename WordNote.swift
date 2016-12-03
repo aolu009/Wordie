d33575a41b525e8d4673b75d6920bdcfcafeb125
aolu009 <aolu009@gmail.com>
2016-12-02 23:32:03 -0800

    123

---
+++ WordNote.swift
@@ -0,0 +1,25 @@
+//
+//  WordNote.swift
+//  Wordie
+//
+//  Created by Lu Ao on 12/2/16.
+//  Copyright © 2016 Lu Ao. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WordNote: NSObject {
+    
+    var word: String?
+    
+    
+    
+    
+    
+    
+    init(dictionary: NSDictionary){
+        
+    }
+}
+
