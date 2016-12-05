76a1ae1709b120de2ebe4a719fe83c92126470c0
parry <parry@parrys-air.attlocal.net>
2016-12-04 18:37:11 -0800

    starting bottom swipe

---
+++ VideoCollectionViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  VideoCollectionViewCell.swift
+//  Wordie
+//
+//  Created by parry on 12/3/16.
+//  Copyright © 2016 Lu Ao. All rights reserved.
+//
+
+import UIKit
+
+class VideoCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var placeholderImageView: UIImageView!
+    var url: URL?
+ 
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder:aDecoder)
+        
+        self.contentView.layer.cornerRadius = 2
+        self.contentView.layer.masksToBounds = true;
+    }
+    
+    
+}
